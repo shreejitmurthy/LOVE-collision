a8f5412da16e433496712902a9b0e926a304b379
Shreejit Murthy <botmorph17@gmail.com>
2024-02-04 21:36:11 +1030

    allow for multiple collisions

--- collision.lua
+++ collision.lua
@@ -148,64 +148,60 @@ function PhysicsWorld:NewEntity(entity_type, x, y, width, height)
 end
 
 function PhysicsWorld:UpdateCollisionDetection(dt)
-    collisionInfo = nil
     local n = #self.entities
+    local collisionInfo = {}
+
     for i = 1, n - 1 do
         for j = i + 1, n do
             local r1 = self.entities[i]
             local r2 = self.entities[j]
-            hit = rectRect(r1.x, r1.y, r1.width, r1.height, r2.x, r2.y, r2.width, r2.height)
+            local hit = rectRect(r1.x, r1.y, r1.width, r1.height, r2.x, r2.y, r2.width, r2.height)
 
-            -- local dynamic_rect = (r1.etype == "dynamic" and r1) or
-            -- (r2.etype == "dynamic" and r2)
-            local dynamic_rect = r1
-            dynamic_rect:Update(dt)
-
-            if dynamic_rect.isIdle == false then
-                if hit then
-                    collisionInfo = {i, j, dynamic_rect.isIdle}
-                    if dynamic_rect then
-                        local dx = (dynamic_rect.x + dynamic_rect.width/2) - (r2.x + r2.width/2)
-                        local dy = (dynamic_rect.y + dynamic_rect.height/2) - (r2.y + r2.height/2)
-                        local overlapX = (dynamic_rect.width + r2.width) / 2 - math.abs(dx)
-                        local overlapY = (dynamic_rect.height + r2.height) / 2 - math.abs(dy)
-    
-                        -- Adjust the position of the dynamic entity
-                        if overlapX < overlapY then
-                            if dx > 0 then
-                                dynamic_rect.x = dynamic_rect.x + overlapX
-                            else
-                                dynamic_rect.x = dynamic_rect.x - overlapX
-                            end
-                        else
-                            if dy > 0 then
-                                dynamic_rect.y = dynamic_rect.y + overlapY
-                            else
-                                dynamic_rect.y = dynamic_rect.y - overlapY
-                            end
-                        end
-                    end
-                    break
-                end
+            if r1.etype == "dynamic" then
+                r1:Update(dt)
             end
 
-        end
-        if collisionInfo then 
-            break 
-        end
-    end
-end
+            if r2.etype == "dynamic" then
+                r2:Update(dt)
+            end
 
-local function getCollisionInfo(collisionInfo)
-    if collisionInfo then
-        if collisionInfo[3] then
-            return "Disabled collision checks"
-        else
-            return string.format("Collision between entities: %d / %d", collisionInfo[1], collisionInfo[2])
+            if hit then
+                local dynamic_rect
+                if r1.etype == "dynamic" then
+                    dynamic_rect = r1
+                elseif r2.etype == "dynamic" then
+                    dynamic_rect = r2
+                end
+
+                if dynamic_rect and not dynamic_rect.isIdle then
+                    local dx = (dynamic_rect.x + dynamic_rect.width / 2) - (r2.x + r2.width / 2)
+                    local dy = (dynamic_rect.y + dynamic_rect.height / 2) - (r2.y + r2.height / 2)
+                    local overlapX = (dynamic_rect.width + r2.width) / 2 - math.abs(dx)
+                    local overlapY = (dynamic_rect.height + r2.height) / 2 - math.abs(dy)
+
+                    -- Adjust the position of the dynamic entity
+                    if overlapX < overlapY then
+                        if dx > 0 then
+                            dynamic_rect.x = dynamic_rect.x + overlapX
+                        else
+                            dynamic_rect.x = dynamic_rect.x - overlapX
+                        end
+                    else
+                        if dy > 0 then
+                            dynamic_rect.y = dynamic_rect.y + overlapY
+                        else
+                            dynamic_rect.y = dynamic_rect.y - overlapY
+                        end
+                    end
+
+                    -- Store collision information
+                    table.insert(collisionInfo, {i, j, dynamic_rect.isIdle})
+                end
+            end
         end
-    else
-        return "No collision detected"
     end
+
+    return collisionInfo
 end
 
 function PhysicsWorld:Draw()
@@ -223,8 +219,6 @@ function PhysicsWorld:Draw()
         fw, fh = font:getWidth(n), font:getHeight()
         love.graphics.print(n, e.x + e.width/2 - fw/2, e.y + e.height/2 - fh/2)
     end
-
-    love.graphics.print(getCollisionInfo(collisionInfo), 10, 10)
 
     love.graphics.reset()
 
